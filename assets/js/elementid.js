d242f53a451648fc0d3dcd55066ac4db3be0d31a
mrmbaja <mbajaustine2gmail.com>
2024-10-21 16:10:39 +0300

    first commit

---
+++ assets/js/elementid.js
@@ -0,0 +1,19 @@
+function elementid() {
+
+}
+
+const colorButton :HTMLElement =document.getElementById(elementid, 'colorButton')
+
+function type() {
+
+}
+
+function listener() {
+
+}
+const addEventListener = colorButton.addEventListener( type, 'click',listener,() :void =>{
+        colorButton.style.backgroundColor=colorButton.style.backgroundColor === 'blue'? '4CAF50':'blue';
+
+    }
+);
+
